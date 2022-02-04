577c4f0d95fc07e9e97d395146bac3b8b8e337aa
Steefan Raveenthiran <s.raveenthiran@gmail.com>
2022-02-04 15:10:52 -0500

    completed chapter 1

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,31 @@
 package main
 
-import "fmt"
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func main() {
-	fmt.Println(Hello("world"))
+
 }
 
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
