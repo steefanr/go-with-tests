fbfdc164d2a71d11b8c945e35f90630d6dd0cbba
Steefan Raveenthiran <s.raveenthiran@gmail.com>
2022-03-23 23:09:39 -0400

    Finished Chapter 9

---
+++ mocking/countdown.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+var finalWord = "Go!"
+
+func Countdown(writer io.Writer, sleeper Sleeper) {
+	for i := 3; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintf(writer, "%d\n", i)
+	}
+	sleeper.Sleep()
+	fmt.Fprintf(writer, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
